be20cc484aaab739b71b3f147f11c101ab0859f6
kenNg1 <kng1230@hotmail.com>
2017-03-24 10:52:01 +0800

    add functionality for exact match and partial match of words

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,1 +1,27 @@
 require ('pry')
+
+class String
+
+  define_method(:word_count_exact) do |word|
+    sentence = self.downcase().split()
+    count = 0
+    sentence.length.times() do |i|
+      if sentence[i].gsub(/[^0-9A-Za-z]/, '') == word
+      count+=1
+      end
+    end
+    count
+  end
+
+  define_method(:word_count_partial) do |word|
+    sentence = self.downcase().split()
+    count = 0
+    sentence.length.times() do |i|
+      if sentence[i].include?("hello")
+      count+=1
+      end
+    end
+    count
+  end
+
+end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -1,2 +1,46 @@
 require ('rspec')
 require ('word_count')
+
+describe('String#word_count_exact') do
+
+  it('identifies that a word appears in a string') do
+    expect("hello ken goodbye".word_count_exact("hello")).to(eq(1))
+  end
+
+  it('returns how freqently a word appears in a string') do
+    expect("hello ken hello goodbye".word_count_exact("hello")).to(eq(2))
+  end
+
+  it('returns how freqently a word appears in a string') do
+    expect("hello hello goodbye hello hello".word_count_exact("hello")).to(eq(4))
+  end
+
+  it('returns how freqently a word appears in a string accounting for punctuation') do
+    expect("hello hello hellogoodbye hello hello!".word_count_exact("hello")).to(eq(4))
+  end
+
+end
+
+describe('String#word_count_partial') do
+
+  it('returns how freqently a word appears in a string accounting for punctuation') do
+    expect("hello hello hellogoodbye hello hello!".word_count_partial("hello")).to(eq(5))
+  end
+
+  it('identifies that a word appears in a string') do
+    expect("hello hello goodbye".word_count_partial("hello")).to(eq(2))
+  end
+
+  it('returns how freqently a word appears in a string') do
+    expect("hello hello goodbye".word_count_partial("hello")).to(eq(2))
+  end
+
+  it('returns how freqently a word appears in a string') do
+    expect("hello hello goodbye hello hello".word_count_partial("hello")).to(eq(4))
+  end
+
+  it('returns how freqently a word appears in a string accounting for punctuation') do
+    expect("hello hello goodbye hello hello!".word_count_partial("hello")).to(eq(4))
+  end
+
+end
